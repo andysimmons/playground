ca6c7924e9aa04d5232c811c7e65dd734a12c4ea
Andy Simmons <ajsimmons@gmail.com>
2017-10-05 13:03:04 -0600

    Update Export-RetinaLog.ps1

--- Export-RetinaLog.ps1
+++ Export-RetinaLog.ps1
@@ -1,89 +1,137 @@
 #requires -Version 5
 <#
-.NOTES
-    Created on:   10/2/2017
-    Created by:   Andy Simmons
-    Organization: St. Luke's Health System
-    Filename:     Export-RetinaLog.ps1
+        .NOTES
+        Created on:   10/2/2017
+        Created by:   Andy Simmons
+        Organization: St. Luke's Health System
+        Filename:     Export-RetinaLog.ps1
 
-.SYNOPSIS
-    Checks an Exchange mailbox for a specific type of message, and if found,
-    exports the message content as a Windows event.
+        .SYNOPSIS
+        Checks an Exchange mailbox for a specific type of message, and if found,
+        exports the message content as a Windows event.
 
-.DESCRIPTION
-	Written to automate some NOC workflows that currently rely on humans
-	monitoring a shared mailbox.
+        .DESCRIPTION
+        Written to automate some NOC workflows that currently rely on humans
+        monitoring a shared mailbox.
 #>
+using namespace Microsoft.Exchange.WebServices.Data
+
 [CmdletBinding(SupportsShouldProcess)]
 param
 (
-	[string] $SqlServer = 'IHTDBA01',
-    
-	[string] $Database = 'NOCRules',
+	[Parameter(Mandatory)]
+	[Net.Mail.MailAddress]
+	$Mailbox,
 
 	[Parameter(Mandatory)]
 	[string]
-	$ProcessedText
+	$FolderName,
+    
+	[int]
+	$ItemLimit = 10,
+    
+	[string]
+	$SqlServer = 'IHTDBA01',
+    
+	[string]
+	$Database = 'NOCRules'
 )
 
 #region Classes
 class MailReader
 {
 	# Properties
+	[Net.Mail.MailAddress] $Mailbox
+	[string] $FolderName
 	[Net.Mail.MailAddress] $Sender
 	[string] $Subject
 	[string] $Body
-	[bool] $MatchesRule
 	[bool] $IsLogged
 	[MailReaderRule[]] $RuleSet
+	[MailReaderRule] $Rule
 
 	# Constructor
-	MailReader([Net.Mail.MailAddress] $Sender, [string] $Subject, [string] $Body, [MailReaderRule[]] $RuleSet) 
+	MailReader (
+		[Net.Mail.MailAddress] $Mailbox, 
+		[string] $FolderName, 
+		[Net.Mail.MailAddress] $Sender, 
+		[string] $Subject, 
+		[string] $Body, 
+		[MailReaderRule[]] $RuleSet
+	) 
 	{ 
-		$this.Sender = $Sender
-		$this.Subject = $Subject
-		$this.Body = $Body
-		
-		$this.RuleSet = $RuleSet.Where({$_.Enabled})
-		$this.MatchesRule = $false
-		$this.IsLogged = $false
+		$this.Mailbox    = $Mailbox
+		$this.FolderName = $FolderName
+		$this.Sender     = $Sender
+		$this.Subject    = $Subject
+		$this.Body       = $Body
+        
+		$this.Initialize($RuleSet)
 	}
-    
+
 	# Methods
+	[void] Initialize ([MailReaderRule[]] $RuleSet) 
+	{
+		$this.IsLogged = $false
+
+		if ($this.Validate())
+		{
+			# Load all enabled rules for this mailbox/folder
+			$this.RuleSet = $RuleSet | Where-Object { 
+				$_.Enabled -and 
+				$_.Mailbox -eq $this.Mailbox -and
+				$_.FolderName -eq $this.FolderName
+			} | Sort-Object -Property 'Priority'
+
+			# Select and assign a rule to this message
+			if ($this.RuleSet) { $this.Rule = $this.FindMatch() }
+		}
+	}
+
 	[bool] Validate ()
 	{
-		# Ensure sender, subject, and body aren't empty, and at least one enabled rule exists
-		return (
-			($this.Sender -and $this.Subject -and $this.Body) -and 
-			$this.RuleSet.Where({$_.Enabled})
-		)
+		return $this.Mailbox -and $this.FolderName -and $this.Sender -and $this.Subject -and $this.Body
 	}
 
-	hidden [bool] TryMatch ([MailReaderRule] $Rule)
+	[MailReaderRule] FindMatch ()
 	{
-		return (
-			($this.Subject -notmatch $rule.ProcessedText) -and
-			($this.Sender -match $rule.SenderPattern) -and
-			($this.Subject -match $rule.SubjectPattern) -and
-			($this.Body -match $rule.BodyPattern)
-		)
+		foreach ($rule in $this.RuleSet)
+		{
+			if ($this.TestMatch($rule)) { return $rule }
+		}     
+
+		return $null
 	}
 
-	[void] Log ([MailReaderRule] $Rule)
+	hidden [bool] TestMatch ([MailReaderRule] $Rule)
+	{
+		# Returns true if this rule matches the message, and the message
+		# hasn't been marked as "processed" already
+		return ( 
+			($this.Subject -notmatch "^$($rule.ProcessedText)") -and
+			($this.Mailbox       -eq $rule.Mailbox) -and
+			($this.FolderName    -eq $rule.FolderName) -and
+			($this.Sender     -match $rule.SenderPattern) -and
+			($this.Subject    -match $rule.SubjectPattern) -and
+			($this.Body       -match $rule.BodyPattern)
+		)
+	}
+    
+	[void] InvokeRule ()
 	{
 		# If the rule specifies an event log message, we'll use that.
 		# If not, we'll use the subject from the mail message.
-		if ($Rule.LogMessage) { $Message = $Rule.LogMessage }
-		else                  { $Message = $this.Subject }
+		if ($this.Rule.LogMessage) { $Message = $this.Rule.LogMessage }
+		else                       { $Message = $this.Subject }
 
 		try
 		{
 			$weParams = @{
-				ComputerName = $Rule.LogServer
-				LogName      = $Rule.LogName
-				Source       = $Rule.LogSource
-				EventId      = $Rule.EventId
-				EntryType    = $Rule.EntryType
+				ComputerName = $this.Rule.LogServer
+				LogName      = $this.Rule.LogName
+				Source       = $this.Rule.LogSource
+				EventId      = $this.Rule.EventId
+				EntryType    = $this.Rule.EntryType
 				Message      = $Message
 				ErrorAction  = 'Stop'
 			}
@@ -101,6 +149,10 @@ class MailReader
 class MailReaderRule
 {
 	# Properties
+	[int]    $Id
+	[string] $Name
+	[Net.Mail.MailAddress] $Mailbox
+	[string] $FolderName
 	[regex]  $SenderPattern
 	[regex]  $SubjectPattern
 	[regex]  $BodyPattern
@@ -112,24 +164,21 @@ class MailReaderRule
 	[string] $LogMessage
 	[string] $ProcessedText
 	[bool]   $Enabled
-	
+	[int]    $Priority
+    
 	# Constructors
-	MailReaderRule ([pscustomobject] $InputObject)
+	MailReaderRule ([object] $InputObject)
 	{
-		$this.SenderPattern = $InputObject.SenderPattern
-		$this.SubjectPattern = $InputObject.SubjectPattern
-		$this.BodyPattern = $InputObject.BodyPattern
-		$this.LogServer = $InputObject.LogServer
-		$this.LogName = $InputObject.LogName
-		$this.LogSource = $InputObject.LogSource
-		$this.EventId = $InputObject.EventId
-		$this.EntryType = $InputObject.EntryType
-		$this.LogMessage = $InputObject.LogMessage
-		$this.ProcessedText = $InputObject.ProcessedText
-		$this.Enabled = $InputObject.Enabled	
+        $o = $InputObject
+        $this.Initialize($o.Id, $o.Name, $o.Mailbox, $o.FolderName, $o.SenderPattern, $o.SubjectPattern, $o.BodyPattern, $o.LogServer, 
+            $o.LogName, $o.LogSource, $o.EventId, $o.EntryType, $o.LogMessage, $o.ProcessedText, $o.Enabled, $o.Priority)
 	}
 
 	MailReaderRule (
+		[int]    $Id,
+		[string] $Name,
+		[Net.Mail.MailAddress] $Mailbox,
+		[string] $FolderName,
 		[regex]  $SenderPattern,
 		[regex]  $SubjectPattern,
 		[regex]  $BodyPattern,
@@ -140,44 +189,99 @@ class MailReaderRule
 		[string] $EntryType,
 		[string] $LogMessage,
 		[string] $ProcessedText,
-		[bool]   $Enabled
+		[bool]   $Enabled,
+		[int]    $Priority
 	)
 	{
-		$this.SenderPattern = $SenderPattern
-		$this.SubjectPattern = $SubjectPattern
-		$this.BodyPattern = $BodyPattern
-		$this.LogServer = $LogServer
-		$this.LogName = $LogName
-		$this.LogSource = $LogSource
-		$this.EventId = $EventId
-		$this.EntryType = $EntryType
-		$this.LogMessage = $LogMessage
-		$this.ProcessedText = $ProcessedText
-		$this.Enabled = $Enabled
+		$this.Initialize($Id, $Name, $Mailbox, $FolderName, $SenderPattern, $SubjectPattern, $BodyPattern, $LogServer, 
+			$LogName, $LogSource, $EventId, $EntryType, $LogMessage, $ProcessedText, $Enabled, $Priority)
 	}
 
-	# No methods
-}
+	# Methods
+	[void] Initialize (
+		[int]    $Id,
+		[string] $Name,
+		[Net.Mail.MailAddress] $Mailbox,
+		[string] $FolderName,
+		[regex]  $SenderPattern,
+		[regex]  $SubjectPattern,
+		[regex]  $BodyPattern,
+		[string] $LogServer,
+		[string] $LogName,
+		[string] $LogSource,
+		[int]    $EventId,
+		[string] $EntryType,
+		[string] $LogMessage,
+		[string] $ProcessedText,
+		[bool]   $Enabled,
+		[int]    $Priority
+	)
+	{
+		$this.Id             = $Id
+		$this.Name           = $Name
+		$this.Mailbox        = $Mailbox
+		$this.FolderName     = $FolderName
+		$this.SenderPattern  = $SenderPattern
+		$this.SubjectPattern = $SubjectPattern
+		$this.BodyPattern    = $BodyPattern
+		$this.LogServer      = $LogServer
+		$this.LogName        = $LogName
+		$this.LogSource      = $LogSource
+		$this.EventId        = $EventId
+		$this.EntryType      = $EntryType
+		$this.LogMessage     = $LogMessage
+		$this.Enabled        = $Enabled
+		$this.Priority       = $Priority
 
+		if ($ProcessedText) { $this.ProcessedText = $ProcessedText }
+		else                { $this.ProcessedText = '[PROCESSED] ' }
+	}
+    
+	[void] RegisterLogSource ()
+	{
+		# Register the log source for this rule (to allow writing to the event log)
+		try 
+		{ 
+			$nelParams = @{
+				LogName      = $this.LogName
+				Source       = $this.LogSource
+				ComputerName = $this.LogServer
+				ErrorAction  = 'Stop'
+			}
+			New-Eventlog @nelParams 
+			Write-Verbose "Registered $($this.LogName) log source '$($this.LogSource)' on $($this.LogServer)."
+		}
+
+		# Redirect error to the verbose stream if the log source already exists
+		catch [InvalidOperationException] { Write-Verbose $_.Exception.Message } 
+        
+		catch { throw $_.Exception }
+	}
+
+	[string] ToString () { return $this.Name }
+}
 #endregion Classes
 
 #region Functions
 <#
 .SYNOPSIS
-    Retrieves message processing rules from a database.
+Retrieves message processing rules from a database.
 
 .PARAMETER SqlServer
-    SQL Server hostname/IP/FQDN
+SQL Server hostname/IP/FQDN
 
 .PARAMETER Database
-    Database name
+Database name
 #>
 function Get-MailReaderRules
 {
 	[CmdletBinding()]
 	param (
 		[string] $SqlServer = 'IHTDBA01',
-		[string] $Database = 'NOCRules'
+		[string] $Database = 'NOCRules',
+		[string] $Mailbox = $Mailbox,
+		[string] $FolderName = $FolderName,
+		[switch] $IncludeDisabled
 	)
 
 	Write-Verbose "Opening connection to $Database database on $SQLServer"
@@ -187,10 +291,11 @@ function Get-MailReaderRules
 	"Initial Catalog=$Database"
 	$connection.Open()
 
-	$query = "select 
+	$query = 'SELECT 
                 Id,
                 RuleName,
                 Enabled,
+                Priority,
                 Mailbox,
                 FolderName,
                 SenderPattern,
@@ -201,9 +306,24 @@ function Get-MailReaderRules
                 LogSource,
                 EventId,
                 EntryType,
-                LogMessage
-            from
-                dbo.MailReaderRules"
+                LogMessage,
+                ProcessedText
+            FROM
+                dbo.MailReaderRules'
+    
+	$ruleFilter = @()
+	if ($Mailbox)          { $ruleFilter += "Mailbox like '$Mailbox'" }
+	if ($FolderName)       { $ruleFilter += "FolderName like '$FolderName'"}
+	if (!$IncludeDisabled) { $ruleFilter += "Enabled = 1" }
+    
+	if ($ruleFilter)
+	{
+		$query += "`nWHERE " + $($ruleFilter -join " AND`n")
+	}
+    
+	$query += "`nORDER BY Priority ASC"
+
+	write-verbose $query
 
 	Write-Verbose 'Loading rules'
 	$cmd = New-Object Data.SqlClient.SqlCommand $query, $connection
@@ -211,10 +331,11 @@ function Get-MailReaderRules
 
 	while ($reader.Read())
 	{
-		[pscustomobject] [ordered] @{
+		$ruleData = [pscustomobject] @{
 			Id             = [int]    $reader.Item('Id')
-			RuleName       = [string] $reader.Item('RuleName')
+			Name           = [string] $reader.Item('RuleName')
 			Enabled        = [bool]   $reader.Item('Enabled')
+			Priority       = [int]    $reader.Item('Priority')
 			Mailbox        = [string] $reader.Item('Mailbox')
 			FolderName     = [string] $reader.Item('FolderName')
 			SenderPattern  = [regex]  $reader.Item('SenderPattern')
@@ -226,7 +347,10 @@ function Get-MailReaderRules
 			EventId        = [int]    $reader.Item('EventId')
 			EntryType      = [string] $reader.Item('EntryType')
 			LogMessage     = [string] $reader.Item('LogMessage')
+			ProcessedText  = [string] $reader.Item('ProcessedText')
 		}
+
+		[MailReaderRule]::New($ruleData)
 	}
 
 	Write-Verbose 'Closing SQL connection'
@@ -237,39 +361,30 @@ function Get-MailReaderRules
 
 <#
 .SYNOPSIS
-    Processes "trigger" email messages.
+Processes "trigger" email messages.
 
 .DESCRIPTION
-    Checks a mailbox for messages that match specific sender and subject
-    patterns, returns a few interesting properties, and deletes those messages.
+Checks a mailbox for messages that match specific sender and subject
+patterns, returns a few interesting properties, and deletes those messages.
 
-    Requires the Exchange Web Services (EWS) API. Tested with v2.2.
+Requires the Exchange Web Services (EWS) API. Tested with v2.2.
 
 .PARAMETER EwsAssembly
-    Exchange Web Services API DLL path
+Exchange Web Services API DLL path
 
 .PARAMETER Credentials
-    Exchange mailbox credentials
+Exchange mailbox credentials
 
 .PARAMETER Mailbox
-    Mail address associated with the mailbox (used for autodiscovery of Exchange server)
+Mail address associated with the mailbox (used for autodiscovery of Exchange server)
 
 .PARAMETER ItemLimit
-    Maximum number of items to retrieve
+Maximum number of items to retrieve
 
-.PARAMETER SenderPattern
-    Regular expression describing eligible "From" email address(es)
-
-.PARAMETER SubjectPattern
-    Regular expression describing eligible "Subject" text
-
-.PARAMETER BodyPattern
-    Regular expression describing eligible "Body" text
-
-.EXAMPLE
-    Receive-TriggerMessage -Mailbox 'joe@abc.tld'
+.PARAMETER RuleSet
+Collection of rules for message processing
 #>
-function Export-TriggerMessage 
+function Export-TriggerMessage
 {
 	[CmdletBinding(SupportsShouldProcess)]
 	param 
@@ -292,17 +407,8 @@ function Export-TriggerMessage
 		[string]
 		$FolderName,
 
-		[Parameter(Mandatory)]
-		[regex]
-		$SenderPattern,
-
-		[Parameter(Mandatory)]
-		[regex]
-		$SubjectPattern,
-
-		[Parameter(Mandatory)]
-		[regex]
-		$BodyPattern
+		[MailReaderRule[]]
+		$RuleSet
 	)
 
 	[void] [Reflection.Assembly]::LoadFile($EwsAssembly)
@@ -323,7 +429,7 @@ function Export-TriggerMessage
 	$resolveConflicts = [Microsoft.Exchange.WebServices.Data.ConflictResolutionMode]::AutoResolve
 
 	# Set the deletion mode to move the message to deleted items
-	$deleteMode = [Microsoft.Exchange.WebServices.Data.DeleteMode]::MoveToDeletedItems
+	#$deleteMode = [Microsoft.Exchange.WebServices.Data.DeleteMode]::MoveToDeletedItems
 
 	Write-Verbose "Retrieving the $ItemLimit most recent messages for $Mailbox ..."
 	$items = $mailFolder.FindItems($ItemLimit)
@@ -332,43 +438,37 @@ function Export-TriggerMessage
 	{
 		$item.Load($desiredProps)
 
-		$itemIsTrigger = ($item.Subject -notmatch "^$ProcessedText") -and 
-		($item.Subject -match $SubjectPattern)      -and
-		($item.Sender -match $SenderPattern)        -and
-		($item.Body -match $BodyPattern)
+		$reader = [MailReader]::New($Mailbox, $FolderName, $item.Sender.Address, $item.Subject, $item.Body.Text, $RuleSet)
         
-		if ($itemIsTrigger)
-		{
-			if ($PSCmdlet.ShouldProcess($item.Subject, 'process'))
+		if ($reader.Rule -and $PSCmdlet.ShouldProcess($reader.Subject, "process rule: $($reader.Rule)"))
+		{ 
+			$reader.InvokeRule()
+            
+			if ($reader.IsLogged) 
 			{
-				Write-Verbose "TRIGGER FOUND! $($item.Subject)"
-				# Return a custom object with some interesting message properties for easy export
-				[pscustomobject] [ordered] @{
-					DateTimeSent     = $item.DateTimeSent
-					DateTimeReceived = $item.DateTimeReceived
-					SenderAddress    = $item.Sender.Address
-					Subject          = $item.Subject
-					Body             = $item.Body.Text
-					InternetHeaders  = $item.InternetMessageHeaders -join "`n"
-				}
-
-				# Prepend the "processed" text to the subject, mark message as read, and delete it.
-				$item.Subject = "$ProcessedText$($item.Subject)"
+				Write-Verbose "Marking item '$($item.Subject)' as processed"
+				$item.Subject = $reader.Rule.ProcessedText + $item.Subject
 				$item.IsRead = $true
 				$item.Update($resolveConflicts)
-				$item.Delete($deleteMode)
 			}
-		}
-		elseif ($item.Subject -match "^$ProcessedText")
-		{
-			Write-Warning "Item may have been partially processed on a previous run. Skipping: $($item.Subject)"
-		}
-		else
-		{
-			Write-Verbose "Skipping item: $($item.Subject)"
-		}
+		}        
 	}
 }
 #endregion Functions
 
-# Retrieve new trigger messages
+
+$ruleSet = @(Get-MailReaderRules)
+
+if ($ruleSet)
+{
+	try { $ruleSet.RegisterLogSource() }
+	catch
+	{ 
+		Write-Error "Failed to register the log source for one or more rules. Aborting"
+		Throw $_.Exception
+	}
+	$reader = Export-TriggerMessage -Mailbox $Mailbox -FolderName $FolderName -RuleSet $ruleSet -ItemLimit $ItemLimit
+	$reader    
+}
+
+
