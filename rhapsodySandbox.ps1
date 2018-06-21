77cc3ecf9e4c99a70a9c0ae61f53954b4b9e7a2e
Andy Simmons <ajsimmons@gmail.com>
2018-06-21 15:25:20 -0600

    Update rhapsodySandbox.ps1
    committing before I nuke a bunch

--- rhapsodySandbox.ps1
+++ rhapsodySandbox.ps1
@@ -1,7 +1,12 @@
 #requires -Module TunableSSLValidator
 #requires -Version 5
+
 using namespace Microsoft.PowerShell.Commands
+using namespace System.Net
 using namespace System.Xml
+using namespace System.Runtime.InteropServices
+using namespace System.Collections.Generic
+
 [CmdletBinding()]
 param (
     [Parameter(Mandatory)]
@@ -9,119 +14,280 @@ param (
     $Credential,
 
     [int[]]
-    $id = 6223..6226,
+    $Id = 6223..6226,
 
     [uri]
-    $BaseURI = 'https://rhapsodyindev01.slhs.org:8444',
+    $BaseUri = 'https://rhapsodyindev01.slhs.org:8444',
 
     [switch]
     $Insecure
 )
 
-<#
-scratch area
-
-https://rhapsodyindev01.slhs.org:8444/api/commpoint/6223
+enum CommPointAction 
 {
-    "data": {
-        "id": 6223,
-        "name": "eICUResTest 1",
-        "folderPath": "Main/zzTesting/Tims/RES Automation Testing",
-        "mode": "OUTPUT",
-        "state": "RUNNING",
-        "commPointType": "Sink",
-        "inputIdleTime": "PT27H48M27.009S",
-        "outQueueSize": 0,
-        "outputIdleTime": "PT27H48M27.009S",
-        "uptime": "PT27H48M27.009S",
-        "connectionCount": 1,
-        "sentCount": 0
-    },
-    "error": null
+    START
+    STOP
+    RESTART
 }
 
-[System.Xml.XmlConvert]::ToTimeSpan("PT27H48M27.009S")
-
-#>
-
-# establish session
-try {
-    $irmParams = @{
-        Credential      = $Credential
-        Uri             = $BaseURI
-        Method          = 'Get'
-        SessionVariable = 'session'
-        Insecure        = $Insecure
-        ErrorAction     = 'Stop'
-    }
-    Invoke-RestMethod @irmParams
-    Write-Verbose "Session established: $BaseURI"
-}
-catch {
-    Write-Error "Couldn't connect to API! URI: $BaseURI"
-    throw $_.Exception
-}
-
-class CommPoint {
+class CommPoint
+{
     # properties
-    [int]               $Id
-    [string]            $Name
-    [string]            $FolderPath
-    [string]            $Mode
-    [string]            $State
-    [string]            $CommPointType
-    [timespan]          $InputIdleTime
-    [int]               $OutQueueSize
-    [timespan]          $OutputIdleTime
-    [timespan]          $Uptime
-    [int]               $ConnectionCount
-    [int]               $SentCount
-    [WebRequestSession] $Session
-    [Uri]               $BaseURI
-
+    hidden [pscredential] $Credential
+    hidden [Uri]          $BaseUri
+    
+    [int]      $Id
+    [string]   $Name
+    [string]   $FolderPath
+    [string]   $Mode
+    [string]   $State
+    [string]   $CommPointType
+    [timespan] $InputIdleTime
+    [int]      $OutQueueSize
+    [timespan] $OutputIdleTime
+    [timespan] $Uptime
+    [int]      $ConnectionCount
+    [int]      $SentCount
+    [bool]     $SkipCertificateCheck
 
     # constructors
     CommPoint (
-        [WebRequestSession] $Session,
-        [Uri]               $BaseURI,
-        [int]               $Id
-    ) {
+        [Uri]          $BaseUri,
+        [int]          $Id,
+        [pscredential] $Credential
+    )
+    {
+        $this.BaseUri = $BaseUri
         $this.Id = $Id
-        $this.Session = $Session
-        $this.BaseURI = $BaseURI
+        $this.Credential = $Credential
+        $this.SkipCertificateCheck = $false
+
+        $this.Initialize()
+    }
+
+    CommPoint (
+        [Uri]          $BaseUri,
+        [int]          $Id,
+        [pscredential] $Credential,
+        [bool]         $SkipCertificateCheck
+    )
+    {
+        $this.BaseUri = $BaseUri
+        $this.Id = $Id
+        $this.Credential = $Credential
+        $this.SkipCertificateCheck = $SkipCertificateCheck
 
         $this.Initialize()
     }
 
     # methods
-    hidden [void] Initialize () {
-
-        try {
-            $irmParams = @{
-                Uri         = $("{0}api/commpoint/{1}" -f $this.BaseURI, $this.Id)
-                WebSession  = $this.Session
-                Method      = 'Get'
-                ErrorAction = 'Stop'
-            }
-            $data = (Invoke-RestMethod @irmParams).Data
-        }
-        catch {
+    hidden [void] Initialize ()
+    {
+        try { $this.Refresh() }
+        catch
+        {
             Write-Error "Initialization failed."
             throw $_.Exception
         }
+    }
 
-        $this.Name = $data.Name
-        $this.FolderPath = $data.folderPath
-        $this.Mode = $data.mode
-        $this.State = $data.State
-        $this.CommPointType = $data.commPointType
-        $this.InputIdleTime = [XmlConvert]::ToTimeSpan($data.inputIdleTime)
-        $this.OutQueueSize = $data.outQueueSize
-        $this.OutputIdleTime = [XmlConvert]::ToTimeSpan($data.outputIdleTime)
-        $this.Uptime = [XmlConvert]::ToTimeSpan($data.uptime)
-        $this.ConnectionCount = $data.connectionCount
-        $this.SentCount = $data.sentCount
+    [void] Refresh ()
+    {
+        try
+        {
+            $irmParams = @{
+                Uri         = $("{0}api/commpoint/{1}" -f $this.BaseUri, $this.Id)
+                Credential  = $this.Credential
+                Method      = 'GET'
+                ErrorAction = 'Stop'
+                Insecure    = $
+            }
+            $data = (Invoke-RestMethod @irmParams).Data
+        }
+        catch { throw $_.Exception }
+
+        # assign properties that can be coerced as-is
+        $usableProps = 'Name', 'FolderPath', 'Mode', 'State', 'CommPointType', 'OutQueueSize', 'ConnectionCount', 'SentCount'
+        $usableProps.ForEach({$this.$_ = $data.$_})
+ 
+        # deserialize timespan properties
+        $xmlTsProps = 'InputIdleTime', 'OutPutIdleTime', 'Uptime'
+        foreach ($prop in $xmlTsProps)
+        {
+            try { $this.$prop = [XmlConvert]::ToTimeSpan($data.$prop) }
+            catch { $this.$prop = 0 }
+        }
+    }
+<#
+    hidden [void] UpdateSession ()
+    {
+        if (!$this.TestSession()) { $this.RepairSession() }
+    }
+
+    hidden [bool] TestSession()
+    {
+        try
+        {
+            $iwrParams = @{
+                Uri         = $this.BaseUri
+                WebSession  = $this.Session
+                Method      = 'GET'
+                ErrorAction = 'Stop'
+            }
+            $response = Invoke-WebRequest @iwrParams
+            $summary = Get-ResponseSummary $response
+
+            if ($response.BaseResponse.StatusDescription -ne 'OK') { throw [ExternalException] $summary }
+            else { return $true }
+        }
+        catch { return $false }
+    }
+
+    hidden [void] RepairSession()
+    {
+        try
+        {
+            $psCreds = $this.Session.Credential | ConvertFrom-NetworkCredential
+            $newSession = New-WebSession -Uri $this.BaseUri -Credential $psCreds -ErrorAction Stop
+
+            if ($newSession) { $this.Session = $newSession }
+        }
+        catch 
+        {
+            Write-Error "Couldn't establish new session with $($this.BaseUri)." 
+            throw $_.Exception
+        }
+    }
+#>
+    hidden [void] Invoke ([CommPointAction] $Action)
+    {
+        $this.Refresh()
+
+        $headers = [Dictionary[[String],[String]]]::new()
+        $headers.Add('Content-Type','text/plain')
+
+        $iwrParams = @{
+            Uri         = $("{0}api/commpoint/{1}/state" -f $this.BaseUri, $this.Id)
+
+            Method      = 'PUT'
+            Body        = $Action
+            ContentType = 'Text/Plain'
+            Credential  = $this.Credential
+            ErrorAction = 'Continue'
+        }
+        
+        $response = Invoke-WebRequest @iwrParams
+        $summary = Get-ResponseSummary $response
+
+        switch ($response.BaseResponse.StatusCode -as [int]) 
+        {
+            204 { Write-Verbose "$Action request accepted. ($summary)" }
+            500 { throw [ExternalException] "State change failed! ($summary)" }
+            400 
+            {
+                $longMessage = "Valid actions are supposedly $([Enum]::GetNames([CommPointAction]) -join ', '), " + 
+                "but the server says it can't $Action communication points. ($summary)"
+                throw [ArgumentOutOfRangeException] $longMessage
+            }
+            default { throw "This wasn't in the docs. Not sure what happened. ($summary)" }
+        }
     }
 }
 
-$id.ForEach({[CommPoint]::new($session, $BaseURI, $_)})
+<#
+function New-WebSession 
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [pscredential]
+        $Credential,
+    
+        [Parameter(Mandatory)]
+        [uri]
+        $Uri,
+    
+        [switch]
+        $Insecure
+    )
+
+    try
+    {
+        $iwrParams = @{
+            Credential      = $Credential
+            Uri             = $Uri
+            Method          = 'Get'
+            SessionVariable = 'session'
+            Insecure        = $Insecure
+            ErrorAction     = 'Stop'
+        }
+        $response = Invoke-WebRequest @iwrParams
+        $summary = Get-ResponseSummary $response
+
+        if ($response.BaseResponse.StatusDescription -ne 'OK') { throw [ExternalException] $summary }
+        else 
+        {
+            Write-Verbose $summary 
+            return $session
+        }
+    }
+    catch
+    {
+        Write-Error "Couldn't connect to API! URI: $BaseUri"
+        throw $_.Exception
+    }
+}
+#>
+function Get-ResponseSummary
+{
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+        [HtmlWebResponseObject[]]
+        $Response
+    )
+
+    process 
+    {    
+        foreach ($r in $Response)
+        {
+            $blanks = @(
+                $r.BaseResponse.ProtocolVersion
+                $r.BaseResponse.StatusCode -as [int]
+                $r.BaseResponse.StatusDescription
+                $r.BaseResponse.Method
+                $r.BaseResponse.ResponseURI
+            )
+
+            '[HTTP/{0} {1} {2}] {3} {4}' -f $blanks
+        }
+    }
+}
+
+function ConvertFrom-NetworkCredential
+{
+    [OutputType([pscredential])]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+        [NetworkCredential[]]
+        $Credential
+    )
+
+    process
+    {
+        foreach ($netCred in $Credential)
+        {
+            if ($netCred.Domain)
+            { 
+                $userName = '{0}\{1}' -f $netCred.Domain, $netCred.User
+            }
+            else { $userName = $netCred.UserName }
+
+            [pscredential]::new($userName, $netCred.SecurePassword)
+        }
+    }
+}
+
+
+$a, $b, $c, $d = $id.ForEach({[CommPoint]::new($BaseUri, $_, $Credential)})
