0f93a3c2b1386326251f23ffc9e22cef3c3d282f
Andy Simmons <ajsimmons@gmail.com>
2019-05-03 10:24:55 -0600

    Update New-SrmSnapshot.ps1
    added comments

--- New-SrmSnapshot.ps1
+++ New-SrmSnapshot.ps1
@@ -9,12 +9,27 @@ param (
     $VMName
 )
 
+# Depending on PowerCLI config, connecting without the -NotDefault triggers
+# some obnoxious warnings. Functionality is the same either way.
 try { Connect-VIServer -Server $VIServer -Force -NotDefault -ErrorAction 'Stop' }
 catch {
     Write-Error "Error connecting to VI server(s): $($VIServer -join ', ')"
     throw $_.Exception
 }
 
+<#
+.SYNOPSIS
+    Filters out SRM placeholder virtual machines from a collection of VMs
+
+.PARAMETER VirtualMachine
+    One or more VM objects to be filtered
+
+.EXAMPLE
+    Get-VM -Server 'vc01','vc02' -Name 'srmprotectedvm' | Skip-PlaceHolder
+
+    Retrieves all virtual machines from "vc01" and "vc02" named "srmprotectedvm",
+    and filters out any placeholder VMs.
+#>
 function Skip-PlaceHolder {
     [CmdletBinding()]
     param (
@@ -33,10 +48,12 @@ function Skip-PlaceHolder {
     }
 }
 
+# get any primary VMs that match the name(s) we're looking for
 $primaryVm = Get-VM -Name $VMName -Server $VIServer | Skip-PlaceHolder
 
+# and snapshot them
 foreach ($vm in $primaryVm) {
     if ($PSCmdlet.ShouldProcess($vm, 'create snapshot')) {
-        New-Snapshot -Name "$[Snapshot Name]" -Description "$[Snapshot Description]" $[SnapMemory_Param] -VM $vm
+        New-Snapshot -Name "Andy Test" -Description "Not important" -VM $vm
     }
 }
