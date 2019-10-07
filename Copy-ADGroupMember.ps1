6f383c8fcbb9789aeb33f9cae1b529f35e7d36d2
Andy Simmons <ajsimmons@gmail.com>
2019-10-07 13:05:55 -0600

    add support for copying >5k members at a time

--- Copy-ADGroupMember.ps1
+++ Copy-ADGroupMember.ps1
@@ -16,6 +16,15 @@
 .PARAMETER DestGroup
     Group into which the members will be copied
 
+.PARAMETER BatchSize
+    Maximum number of members to add to a group in a single operation. All users
+    will still be copied, but will be broken up into batches no larger than this.
+
+    AD web services tends to freak out around 5k users per operation.
+
+.PARAMETER Interval
+    Time (in seconds) to wait between batches.
+
 .EXAMPLE
     .\Copy-ADGroupMember.ps1 -SourceGroup 'GroupA' -DestGroup 'GroupB'
 
@@ -29,29 +38,83 @@ param (
 
     [Parameter(Mandatory)]
     [string]
-    $DestGroup
+    $DestGroup,
+
+    [int]
+    $BatchSize = 1000,
+
+    [int]
+    $Interval = 5
 )
 
-Write-Verbose "Checking membership on $SourceGroup"
+function Add-ThrottledADGroupMember {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $Identity,
 
+        [Parameter(Mandatory)]
+        [object[]]
+        $Members,
+
+        [int]
+        $BatchSize = 1000,
+
+        [int]
+        $Interval = 5
+    )
+
+    if ($Members.Count -le $BatchSize) {
+        Write-Output "[$(Get-Date -f G)] Adding $($Members.Count) members to $Identity."
+        Add-ADGroupMember -Identity $Identity -Members $Members
+    }
+    else {
+        # Too many for one batch. Split this into two collections (one for now, one for later)
+        $leftovers = $Members | Select-Object -Skip $BatchSize
+        $Members = $Members | Select-Object -First $BatchSize
+
+        Write-Output "[$(Get-Date -f G)] Adding $BatchSize members to $Identity."
+        Add-ADGroupMember -Identity $Identity -Members $Members
+
+        Write-Output "[$(Get-Date -f G)] Waiting $Interval seconds to add remaining $($leftovers.Count) members."
+        Start-Sleep -Seconds $Interval
+        $atadgmParams = @{
+            Identity  = $Identity
+            Members   = $leftovers
+            BatchSize = $BatchSize
+            Interval  = $Interval
+        }
+        Add-ThrottledADGroupMember @atadgmParams
+    }
+}
+
+Write-Output "[$(Get-Date -f G)] Checking membership on $SourceGroup"
 try {
-    # Get-ADGroupMember can't deal with large groups, but we can work around it
-    # with Get-ADGroup.
+    # Get-ADGroupMember barfs if a group contains more members than AD web services can return (default
+    # is 5k at the time of writing), but we can work around that with Get-ADGroup
     $sourceADGroup = Get-ADGroup -Identity $SourceGroup -ErrorAction 'Stop' -Properties 'Members'
-    $destADGroup = Get-ADGroup -Identity $DestGroup -ErrorAction 'Stop'
+    $destADGroup = Get-ADGroup -Identity $DestGroup -ErrorAction 'Stop' -Properties 'Members'
 }
 catch {
-    Write-Error 'Barfed retrieving AD groups. Bailing.'
+    Write-Error "[$(Get-Date -f G)] Barfed retrieving AD groups. Bailing."
     throw $_.Exception
 }
 
-$memberCount = $sourceADGroup.Members.Count
+$newMember = $sourceADGroup.Members.Where({ $_ -notin $destADGroup.Members })
+$newMemberCount = $newMember.Count
 
-if (-not $memberCount) {
-    Write-Warning "Source group '$SourceGroup' has no members. Bailing."
+if (-not $newMemberCount) {
+    Write-Warning "[$(Get-Date -f G)] Source group '$SourceGroup' has no members that aren't already in '$DestGroup'. Nothing to do."
     exit 0
 }
 
-if ($PSCmdlet.ShouldProcess($DestGroup, "copy $memberCount members from $SourceGroup")) {
-    Add-ADGroupMember -Identity $destADGroup -Members $sourceADGroup.Members
+if ($PSCmdlet.ShouldProcess($DestGroup, "copy $newMemberCount members from $SourceGroup")) {
+    $atadgmParams = @{
+        Identity  = $destADGroup
+        Members   = $newMember
+        BatchSize = $BatchSize
+        Interval  = $Interval
+    }
+    Add-ThrottledADGroupMember @atadgmParams
 }
