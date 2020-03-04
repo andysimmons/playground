b33b9847fbd6361932ab73e80957808f9565f141
Andy Simmons <ajsimmons@gmail.com>
2020-03-04 11:28:16 -0700

    Create Test-PS.ps1

---
+++ Test-PS.ps1
@@ -0,0 +1,125 @@
+using namespace System.Management.Automation
+#Requires -Version 5
+<#
+.NOTES
+    Name:   Test-PS.ps1
+    Author: Andy Simmons
+    Date:   3/4/2020
+
+.SYNOPSIS
+    Dummy script to test PS syntax highlighting and PS Host rendering.
+
+.DESCRIPTION
+    Implements most of the stuff I'd like to see to from a PowerShell syntax highlighter in
+    an editor, CLI, and at runtime.
+
+    I haven't found VS Code themes that do a great job differentiating PS output streams,
+    generating legible progress bars, and emphasizing elements I typically care about
+    inside script logic, so I wrote this to see that all at a glance.
+
+.PARAMETER Duration
+    Duration (sec) to display a progress bar
+
+.EXAMPLE
+    1..5 | Test-PS.ps1 -Verbose
+    
+    Generates 5 different progress bars that respectively take 1, 2, 3, 4, and 5 
+    seconds each to complete, outputting a dummy object between each.
+    
+    Incidentally, this also tests some rendering behavior with the PS host. I noticed
+    some funk with VS Code's PowerShell Integrated Console...
+    
+    It looks like messages sent in rapid sequence to different streams often render out of order,
+    and the progress bar will always scroll out of view as new objects hit the output stream.
+    
+    Microsoft's other PS hosts don't seem to be affected (powershell.exe and ISE).
+ #>
+[CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+param (
+    [Parameter(ValueFromPipeline)]
+    [ValidateScript({ $_ -ge 1 })]
+    [int[]] $Duration = 5
+)
+
+begin {
+    <#
+    .SYNOPSIS
+        Prepends a timestamp on a string.
+     #>
+    function Add-TimeStamp {
+        param (
+            [Parameter(ValueFromPipeline, Position = 0)]
+            [string[]]
+            $Message,
+
+            [string]
+            $Format = 'MMM dd hh:mm:ss.fffffff tt'
+        )
+        process { $Message.ForEach({ "[$(Get-Date -f $Format)] $_" }) }
+    }
+
+    # dummy class to generate progress bars and return objects
+    class DummyClass {
+
+        # properties
+        [int] $Duration
+        [string] $Noun
+        [string] $Verb
+        [string] $Status
+
+        # constructors
+        DummyClass ()                { $this.Initialize(10) }
+        DummyClass ([int] $Duration) { $this.Initialize($Duration) }
+
+        # methods
+        hidden [void] Initialize ([int] $Duration) { 
+            $this.Duration = $Duration
+            $this.Status = 'Ready' 
+            $this.Noun = 'dummy object'
+            $this.Verb = 'Wait around for {0} seconds' -f $this.Duration
+        }
+
+        [string[]] GetAction () { return @($this.Noun, $this.Verb) }
+
+        [DummyClass] Invoke () {
+            $this.Status = 'Running'
+
+            # generate a progress bar that takes the specified duration (sec) to
+            # complete, and updates every 100 ms
+            foreach ($i in 1..($this.Duration * 10)) {
+                $wpParams = @{
+                    Activity         = $this.Verb
+                    Status           = $this.Status
+                    PercentComplete  = 10 * $i / $this.Duration
+                    SecondsRemaining = $this.Duration - ($i / 10)
+                }
+                Write-Progress @wpParams
+                Start-Sleep -Milliseconds 100
+            }
+
+            $this.Status = 'Complete'
+            return $this
+        }
+    }
+
+    # test escape char highlighting
+    if ($ProgressPreference -eq [ActionPreference]::SilentlyContinue) {
+        Write-Warning "`$ProgressPreference is $ProgressPreference. You won't see much."
+    }
+}
+
+process { 
+    foreach ($d in $Duration) {
+        $dummy = [DummyClass]::new($d) 
+        if ($PSCmdlet.ShouldProcess( @($dummy.GetAction()) )) { $dummy.Invoke() }
+    }
+}
+
+end { 
+    # TODO: figure out why VS Code's integrated console sometimes renders these out of sequence
+    Add-TimeStamp "001 - Testing output streams. Here's (forced) verbose." | Write-Verbose -Verbose
+    Add-TimeStamp "002 - This is standard output." | Write-Output
+    Add-TimeStamp "003 - Here's a warning!"  | Write-Warning
+    Add-TimeStamp "004 - HERE'S AN ERROR!" | Write-Error
+    Add-TimeStamp "005 - Script finished." | Write-Verbose
+}
